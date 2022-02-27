96d94b2361740f11caa79c3300f423f3abde90ec
Jair Partida <jairpartida1@gmail.com>
2022-02-26 22:15:56 -0700

    starting the project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import WebFont from 'webfontloader'
+import Container from './elements/Container'
+import {BrowserRouter, Route, Switch} from 'react-router-dom' 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+WebFont.load({
+  google: {
+    families: ['Manrope:400,500,600,700,800', 'sans-serif']
+  }
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Index = () => {
+  return (
+    <BrowserRouter>
+      <Container>
+        <App />
+      </Container>
+    </BrowserRouter>
+    
+  )
+}
+
+ReactDOM.render(<Index />, document.getElementById('root'))
